9e5ef045138f5805c4e188945a53ebff3130613c
luyoutao <luyoutao@sas.upenn.edu>
2021-07-09 09:46:01 -0400

    fixed comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-#' This Source Code Form is subject to the terms of the Mozilla Public
-#' License, v. 2.0. If a copy of the MPL was not distributed with this
-#' file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#'
-#' Youtao Lu@Kim Lab, 2016-2020
-#' Kim Laboratory Bioinformatics Infrastructure
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Youtao Lu@Kim Lab, 2016-2021
 
 use chrono::Local;
 use env_logger::{self, Builder};
